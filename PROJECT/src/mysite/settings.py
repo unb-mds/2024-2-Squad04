25a3d103723f9a63ae0bedd0b2d7d9bfea5f708c
VictorPontual <victorpontualgan@gmail.com>
2024-11-16 13:59:20 -0300

    <feat>[src]:<criar app e pasta de template base>

--- PROJECT/src/mysite/settings.py
+++ PROJECT/src/mysite/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -31,6 +32,11 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    #My apps
+    'personal',
+
+
+    #Django base apps
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -54,7 +60,7 @@ ROOT_URLCONF = 'mysite.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- PROJECT/src/mysite/urls.py
+++ PROJECT/src/mysite/urls.py
@@ -16,7 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from personal.views import (
+    home_page_view,
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', home_page_view, name= "home"),
 ]
