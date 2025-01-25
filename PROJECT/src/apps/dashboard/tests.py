f010428e26d829dfba0d4bb2403f322e8eda4065
VictorPontual <victorpontualgan@gmail.com>
2025-01-25 15:33:24 -0300

    refactor [APPS]: criar pastas para arquivos de teste em cada app

--- PROJECT/src/apps/dashboard/tests.py
+++
@@ -1,5 +0,0 @@
-from django.test import TestCase
-
-class DashboardTests(TestCase):
-    def test_example(self):
-        self.assertEqual(1 + 1, 2)
---
+++ PROJECT/src/apps/dashboard/tests/tests.py
@@ -0,0 +1,5 @@
+from django.test import TestCase
+
+class DashboardTests(TestCase):
+    def test_example(self):
+        self.assertEqual(1 + 1, 2)
